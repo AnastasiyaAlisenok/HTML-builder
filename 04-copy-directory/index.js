19b43c9a2f24c04aa9f9e9fd743154fea6463b30
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-05-03 23:28:28 +0300

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs/promises');
+const path = require('path');
+const filesFolder = 'C:/Users/HP/HTML-builder/HTML-builder/04-copy-directory/files'
 
+async function copyDir() {
+  try {
+    const createDir = await fs.mkdir('./04-copy-directory/files-copy', { recursive: true });
+    const files = await fs.readdir(filesFolder, { withFileTypes: true } );
+
+    for (const file of files) {
+     fs.copyFile(`./04-copy-directory/files/${file.name}`, `./04-copy-directory/files-copy/${file.name}`);
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+copyDir()
