e1ac88bcdeb9524ef970bca818454c21a8841e4f
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-05-03 17:19:44 +0300

    feat: add solution 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,17 +1,27 @@
 const fs = require('fs');
-const process = require('process');
-const readline = require('readline');
-const {
-  stdin: input,
-  stdout: output,
-} = require('process');
-const rl = readline.createInterface({ input, output });
-const greeting = 'Здравствуйте! Введите текст.'
+const readline = require('node:readline');
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+const greeting = 'Здравствуйте! Введите текст: ';
+const buy = 'Спасибо! Успехов в учебе!';
 
-fs.writeFile('file.txt', greeting, (err) => {
-  if (err) console.log(err);
-    rl.question(greeting, (answer) => {
-      console.log(answer);
-      rl.close();
-    })
+fs.writeFile('02-write-file/newtext.txt', ' ', (error) => {
+  if (error) throw error;
+});
+
+let writeableStream = fs.createWriteStream('02-write-file/newtext.txt');
+console.log(greeting);
+
+rl.on('line', (answer) => {
+  if (answer.toString().trim() === 'exit') {
+    console.log(buy);
+    rl.close();
+  } else {
+    writeableStream.write(`${answer}\n`)
+  }
 })
+
+rl.on('SIGINT', () => {
+    console.log(buy);
+    rl.close()
+})
+
