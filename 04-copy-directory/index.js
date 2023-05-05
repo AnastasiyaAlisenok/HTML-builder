b644a30afc1aa051199ae2c693b27fbd1f8416c9
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-05-06 00:57:07 +0300

    fix: correct function with recursive

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,29 @@
+const { error } = require('console');
 const fs = require('fs/promises');
 const path = require('path');
-const filesFolder = path.join(__dirname, 'files');
-const filesCopyFolder = path.join(__dirname, 'files-copy');
+let filesFolder = path.join(__dirname, 'files');
 
-async function copyDir() {
-  try {
-    const createDir = await fs.mkdir(filesCopyFolder, { recursive: true });
-    const files = await fs.readdir(filesFolder, { withFileTypes: true } );
 
+async function copyDir(folder, newFolder) {
+  try{
+    newFolder = folder.replace('files', 'files-copy');
+    const remove = await fs.rm(newFolder, {recursive: true, force: true}, (err) => {
+      if (err) throw err});
+    const createDir = await fs.mkdir(newFolder, { recursive: true });
+    const files = await fs.readdir(folder, { withFileTypes: true } );
     for (const file of files) {
-     fs.copyFile(`${filesFolder}/${file.name}`, `${filesCopyFolder}/${file.name}`);
+      const stats = await fs.stat(path.join(folder, `${file.name}`));
+      if (stats.isDirectory()) {
+          copyDir(`${folder}/${file.name}`);
+
+        } else {
+          fs.writeFile(newFolder + '\\' + file.name, '');
+          fs.copyFile(`${folder}/${file.name}`, `${newFolder}/${file.name}`);
+        }
     }
-  } catch (err) {
-    console.error(err.message);
-  }
+} catch (err) {
+        console.error(err.message);
+      }
 }
-copyDir()
+
+copyDir(filesFolder)
